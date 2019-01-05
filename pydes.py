f0690a2d1d596e36ac1ad5495bbdd18ce5b84c5c
Nuno <nunolopess@protonmail.com>
2019-01-05 19:40:38 +0000

    Problema de cifrar

--- pydes.py
+++ pydes.py
@@ -1,4 +1,5 @@
 import itertools
+import struct
 #-*- coding: utf8 -*-
 
 #Initial permut matrix for the datas
@@ -31,71 +32,71 @@ CP_2 = [14, 17, 11, 24, 1, 5, 3, 28,
 
 #Expand matrix to get a 48bits matrix of datas to apply the xor with Ki
 E = [32, 1, 2, 3, 4, 5,
-     4, 5, 6, 7, 8, 9,
-     8, 9, 10, 11, 12, 13,
-     12, 13, 14, 15, 16, 17,
-     16, 17, 18, 19, 20, 21,
-     20, 21, 22, 23, 24, 25,
-     24, 25, 26, 27, 28, 29,
-     28, 29, 30, 31, 32, 1]
+    4, 5, 6, 7, 8, 9,
+    8, 9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32, 1]
 
 #SBOX ja trocadas
 S_BOX = [
-         
+
 [[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
- [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
- [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
- [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9],
+    [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+    [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+    [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9],
 ],
 
 [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
- [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
- [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
- [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13],
+    [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+    [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+    [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13],
 ],
 
 [[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
- [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
- [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
- [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12],
+    [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+    [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+    [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12],
 ],
 
 [[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
- [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
- [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
- [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14],
-],  
+    [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+    [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+    [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14],
+],
 
 [[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
- [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
- [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
- [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3],
-], 
+    [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+    [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+    [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3],
+],
 
 [[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
- [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
- [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
- [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13],
-], 
+    [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+    [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+    [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13],
+],
 
 [[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
- [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
- [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
- [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12],
+    [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+    [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+    [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12],
 ],
-   
+
 [[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
- [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
- [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
- [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11],
+    [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+    [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+    [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11],
 ]
 ]
 
 #Permut made after each SBox substitution for each round
 P = [16, 7, 20, 21, 29, 12, 28, 17,
-     1, 15, 23, 26, 5, 18, 31, 10,
-     2, 8, 24, 14, 32, 27, 3, 9,
-     19, 13, 30, 6, 22, 11, 4, 25]
+    1, 15, 23, 26, 5, 18, 31, 10,
+    2, 8, 24, 14, 32, 27, 3, 9,
+    19, 13, 30, 6, 22, 11, 4, 25]
 
 #Final permut for datas after the 16 rounds
 PI_1 = [40, 8, 48, 16, 56, 24, 64, 32,
@@ -118,7 +119,7 @@ def string_to_bit_array(text):#Convert a string into a list of bits
     return array
 
 def bit_array_to_string(array): #Recreate the string from the bit array
-    res = ''.join([chr(int(y,2)) for y in [''.join([str(x) for x in bytes]) for bytes in  nsplit(array,8)]])   
+    res = ''.join(str(e) for e in array)
     return res
 
 def binvalue(val, bitsize): #Return the binary value as a string of the given size 
@@ -147,14 +148,18 @@ class des():
         self.text = text
         
         self.generatechaves() #Generate all the chaves
+        
         #text_blocks = nsplit(self.text, 8) #Split the text in blocks of 8 bytes so 64 bits
         result = list()
+        
         block=self.text
         #TODO
         
         #block = string_to_bit_array(block)#Convert the block in bit array
-
+        #if action==DECRYPT:
+        
         block = self.permut(block,PI)#Apply the initial permutation
+        
         g, d = nsplit(block, 32) #g(LEFT), d(RIGHT)
         tmp = None
         for i in range(16): #Do the 16 rounds
@@ -169,13 +174,10 @@ class des():
             g = d
             d = tmp
         result += self.permut(d+g, PI_1) #Do the last permut and append the result to result
-
+        
         final_res = bit_array_to_string(result)
-        if padding and action==DECRYPT:
-            
-            return self.removePadding(final_res) #Remove the padding if decrypt and padding is true
-        else:
-            return final_res #Return the final string of data ciphered/deciphered
+
+        return final_res #Return the final string of data ciphered/deciphered
     
     def substitute(self, d_e):#Substitute bytes using SBOX
         subblocks = nsplit(d_e, 6)#Split bit array into sublist of 6 bits
@@ -188,10 +190,10 @@ class des():
             bin = binvalue(val, 4)#Convert the value to binary
             result += [int(x) for x in bin]#And append it to the resulting list
         return result
-        
+
     def permut(self, block, table):#Permut the given block using the given table (so generic method)
         return [block[x-1] for x in table]
-    
+
     def expand(self, block, table):#Do the exact same thing than permut but for more clarity has been renamed
         return [block[x-1] for x in table]
     
@@ -202,7 +204,9 @@ class des():
         self.chaves = []
         #key = string_to_bit_array(self.password)
         key=self.password#TODO
+        
         key = self.permut(key, CP_1) #Apply the initial permut on the key
+        
         g, d = nsplit(key, 28) #Split it in to (g->LEFT),(d->RIGHT)
         for i in range(16):#Apply the 16 rounds
             g, d = self.shift(g, d, SHIFT[i]) #Apply the shift associated with the round (not always 1)
@@ -212,20 +216,11 @@ class des():
     def shift(self, g, d, n): #Shift a list of the given value
         return g[n:] + g[:n], d[n:] + d[:n]
     
-    def addPadding(self):#Add padding to the datas using PKCS5 spec.
-        pad_len = 8 - (len(self.text) % 8)
-        self.text += pad_len * chr(pad_len)
-    
-    def removePadding(self, data):#Remove the padding of the plain text (it assume there is padding)
-        pad_len = ord(data[-1])
-        return data[:-pad_len]
-    
     def encrypt(self, key, text, padding=False):
         return self.run(key, text, ENCRYPT, padding)
     
     def decrypt(self, key, text, padding=False):
         tmp= self.run(key, text, DECRYPT, padding)
-        print("decifrando ",tmp)
         return tmp
     
 
@@ -248,10 +243,16 @@ m1=map(int,m1)
 c1=map(int,c1)
 m2=map(int,m2)
 c2=map(int,c2)
-m1.insert(0,0)
-c1.insert(0,0)
-m2.insert(0,0)
-c2.insert(0,0)
+
+while len(m1) !=64:
+    m1.insert(0,0)
+while len(c1) !=64:
+    c1.insert(0,0)
+while len(m2) !=64:
+    m2.insert(0,0)
+while len(c2) !=64:
+    c2.insert(0,0)
+
 
 n = 16
 uns='1'*48
@@ -269,11 +270,14 @@ for i in range(0,size):
         chaves=list(chavestmp[i])
         chaves=map(int,chaves)
         d=des()
+        
+        #print("cifra ", chaves)
+        
         cifras=d.encrypt(chaves,m1)
-        print("cifra ", cifras)
+        print("cifra ", len(cifras))
         decifras=d.decrypt(chaves,c1)
-        print("decifras ",decifras)
-        break
+        print("decifras ",len(decifras))
+        
         if cifras==decifras:
             print(1)
             tmp1=d.encrypt(chaves,m2)
